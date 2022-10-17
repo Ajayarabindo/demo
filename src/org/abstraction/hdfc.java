8fb64adb16f1b3bd217ceee731fc4fef9c0c789f
Ajayarabindo <ajayooredoosim@gmail.com>
2022-10-17 21:03:08 +0300

    ajay code

--- src/org/abstraction/hdfc.java
+++ src/org/abstraction/hdfc.java
@@ -9,6 +9,7 @@ public class hdfc extends Rbi {
 	@Override
 	public void abs() {
 		System.out.println("this is abstraction");
+		System.out.println("This is override");
 	}
 	public static void main(String[] args) {
 		hdfc h = new hdfc();
